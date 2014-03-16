b74f4f4749cd03112c646ab1193541b77e4af6a9
inp2 <Nkechinyere@qchost237-184.cs.illinois.edu>
2014-03-16 13:38:44 -0500

    second commit

---
+++ app/src/main/java/edu/CS463/trafficstats/app/MainActivity.java
@@ -0,0 +1,44 @@
+package edu.CS463.trafficstats.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.ToggleButton;
+import android.view.View;
+
+
+public class MainActivity extends Activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onToggleClicked(View view)
+    {
+        // Is the toggle on?
+        boolean on = ((ToggleButton) view).isChecked();
+
+        if (on)
+        {
+            // Enable vibrate
+        }
+        else
+        {
+            // Disable vibrate
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+}
